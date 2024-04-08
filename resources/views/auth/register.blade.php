d8afe2d029df7f3620c1d89bd6683ef4d5742bd3
rosemarie <rfaller989@gmail.com>
2024-04-08 13:17:27 +0800

    add

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,8 +39,8 @@
 						Welcome
 					</span>
 					<span class="login100-form-title p-b-48">
-						<i class="zmdi zmdi-font"></i>
-					</span> 
+                        <i class="zmdi zmdi-account"></i>
+                    </span>
                     <div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
 						<input class="input100" type="text" name="user_name">
 						<span class="focus-input100" data-placeholder="Name"></span>
